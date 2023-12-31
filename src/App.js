c413d4ec3c217903753cae2c2c8c31c27431fb10
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-31 16:31:18 +0200

    comp

--- src/App.js
+++ src/App.js
@@ -4,22 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
     </div>
   );
 }
 
+function MyComponent(){
+
+  return(
+    <div className='myComponent'>
+      <h2>My React Component</h2>
+    </div>
+  )
+}
 export default App;
